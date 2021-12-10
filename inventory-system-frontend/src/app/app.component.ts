ad3e4e1057e2d63cd02cef2a29ed320b6e4bce84
StefanaAndreeva <stefana.andreeva@gmail.com>
2021-12-10 18:35:39 +0200

    chore(*): add some test functionality for backend integration

--- inventory-system-frontend/src/app/app.component.ts
+++ inventory-system-frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inventory-system-frontend';
+  content = '';
+
+  constructor(private http: HttpClient) {}
+
+  getResource(arg: string) {
+    const endpoint = `/api/v1/${arg}`;
+    this.http
+      .get(endpoint)
+      .pipe(take(1))
+      .subscribe((result) => {
+        this.content = JSON.stringify(result);
+      });
+  }
 }
