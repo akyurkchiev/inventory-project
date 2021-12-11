2a9de1d434c15c1d87e0eb654ec69adcf28f008e
StefanaAndreeva <stefana.andreeva@gmail.com>
2021-12-11 21:29:39 +0200

    add ngprime components library

--- inventory-system-frontend/src/app/app.module.ts
+++ inventory-system-frontend/src/app/app.module.ts
@@ -5,23 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { CoreModule } from './core/core.module';
 import { AssetsModule } from './features/assets/assets.module';
 import { AccountsModule } from './features/accounts/accounts.module';
 
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
     AssetsModule,
-    AccountsModule
+    AccountsModule,
+    ButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
